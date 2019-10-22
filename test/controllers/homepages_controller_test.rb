c040567f4d77b9c153b41420463ee7cdab79c895
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 16:52:16 -0700

    added homepage test

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe HomepagesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  it "can get homepage" do
+    get root_path
+    must_respond_with :success
+  end
+  
 end
