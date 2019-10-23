8043cbdad52daab7a85e008836697b7e205878b2
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 21:03:10 -0700

    fix typo

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-  has_many :products
   has_and_belongs_to_many :products
   
   # VALIDATIONS?  Not sure, probably presence of name since that's the only attribute...
