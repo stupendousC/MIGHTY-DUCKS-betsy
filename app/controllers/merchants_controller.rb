9aec4b34f49ad29bdd24550fe9cf26c71d38af74
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-23 11:29:18 -0700

    added more gemfile for omniauth

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,39 +1,41 @@
 class MerchantsController < ApplicationController
-
+  
   def index
     ### IS THIS NECESSARY???
   end
-
+  
   def new
   end
-
+  
   def create
+    auth_hash = request.env["omniauth.auth"]
+    raise
   end
-
+  
   def login
     # if github authenticates Merchant
-      # if new merchant
-        # call .new and .create
-        # set session
-      # if existing/returning merchant
-        # set session
-        
+    # if new merchant
+    # call .new and .create
+    # set session
+    # if existing/returning merchant
+    # set session
+    
     # else bogus merchant
-      # error page
+    # error page
   end
-
+  
   def edit
   end
-
+  
   def update
   end
-
+  
   def show
   end
-
+  
   def destroy
   end
-
+  
   def logout
   end
   
