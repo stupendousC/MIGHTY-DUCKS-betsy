bdb612d635da6dad42a39e6275a3dbb177b34293
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-10-23 11:06:58 -0700

    Added Order Model validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,11 @@ class Order < ApplicationRecord
   
   has_many :order_items
   
-  #VALIDATIONS
-  # 1.  must have 1 or more items
-  
-  
+  validates :order_items, presence: true
   
   
   private
+  
   def get_grand_total
     # I think it goes here b/c business logic?
     # call this only upon payment? at same time as flipping status from 'pending' to 'done'?
