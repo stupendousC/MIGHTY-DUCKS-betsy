59b0544ee700e68e3a94dc10e9892c34da34c967
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 21:02:17 -0700

    tested ctrller for success in getting view_cart and checkout pages

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  
+  describe "VIEW_CART" do
+    it "can go to view_cart page" do
+      get view_cart_path
+      must_respond_with :success
+    end
+  end
+  
+  describe "CHECKOUT" do
+    it "can go to checkout page" do
+      get checkout_path
+      must_respond_with :success
+    end
+    
+  end
+  
 end
