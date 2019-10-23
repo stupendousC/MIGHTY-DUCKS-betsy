6b62c3e1e96dd0527ea68d9109bff961091a5281
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 17:30:52 -0700

    made & tested Products#index

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can show product#index page" do
+    get products_path
+    must_respond_with :success
+  end
 end
