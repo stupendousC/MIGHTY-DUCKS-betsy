3f7da5618e4a18f73a7957ab9b12ca175bae0a70
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 20:15:07 -0700

    added blank def get_grand_total()

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,13 @@ class Order < ApplicationRecord
   #VALIDATIONS
   # 1.  must have 1 or more items
   
+  
+  
+  
+  private
+  def get_grand_total
+    # I think it goes here b/c business logic?
+    # call this only upon payment? at same time as flipping status from 'pending' to 'done'?
+  end
+  
 end
