e57344c66d3a4b0743fbaf4f5cb5e789c9869e61
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 19:25:16 -0700

    Merchant model ERD association estab'd, TODO: validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,9 @@
 class Merchant < ApplicationRecord
+  has_many :products
+  
+  # VALIDATIONS: see Trello
+  # 1.  Username must be present
+  # 2.  Username must be unique
+  # 3.  Email address must be present
+  # 4.  Email address must be unique
 end
