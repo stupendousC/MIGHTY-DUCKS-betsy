c6ca018b2986b2e4505c4844442bef32bb2b9e40
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-22 20:42:44 -0700

    added Orders ctrller and routes for view_cart & checkout

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   root to: 'homepages#index'
   
   resources :products
-  resources :merchants
   
+  resources :merchants
   get "/login", to: "merchants#login_form", as: "login"
   post "/login", to: "merchants#login"
   post "/logout", to: "merchants#logout", as: "logout"
+  
+  get "/orders/view_cart", to: "orders#view_cart", as: "view_cart"
+  get "/orders/checkout", to: "orders#checkout", as: "checkout"
+  
+  ### did NOT make a route for reviews b/c we can just embed both all the reviews AND the new review form in the products/:id show page?
   
 end
