406b27171598de5e9f42c613815cabdc28236a5d
Diana <dianah46@gmail.com>
2019-10-25 15:17:39 -0700

    created product new form view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :require_user, only: [:new, :create, :edit, :update]
+  before_action :require_login, only: [:edit, :update]
 
   def index 
     #status: nil by default
