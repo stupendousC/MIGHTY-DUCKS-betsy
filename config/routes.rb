f94e3aac24c0ad2f36f9072c8b825325cfd76466
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-25 14:55:00 -0700

    nested routes for Merchants/:id/products and Merchants/:id/orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,20 @@ Rails.application.routes.draw do
   
   resources :products
   
-  resources :merchants, except: [:new, :create]
+  resources :merchants, except: [:new, :create] do
+    resources :products, only: [:index] 
+    resources :orders, only: [:index]
+  end
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#login", as: "auth_callback"
   delete "/logout", to: "merchants#logout", as: "logout"
   # not sure if we need a merchants#index page, why would they want to see what other merchants are out there?
   # merchant's own merchants/:id show page will have all the links to add/edit/delete products? and links to all relevant placed orders?
-
+  
   resources :orders do
     resources :order_items
   end
-
+  
   get "/orders/view_cart", to: "orders#view_cart", as: "view_cart"
   get "/orders/checkout", to: "orders#checkout", as: "checkout"
   post "/orders/purchase", to: "orders#purchase", as: "purchase"
