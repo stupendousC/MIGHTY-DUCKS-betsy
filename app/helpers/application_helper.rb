c849047a003fd1b86317de18c55d2563ab43415f
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-27 18:43:50 -0700

    small things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,9 +47,5 @@ module ApplicationHelper
       return "INVALID"
     end
   end
-
-  def is_a_valid_email?(email)
-    (email =~ /^(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+@{1}((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,4}$/i)
-  end
 end
 
