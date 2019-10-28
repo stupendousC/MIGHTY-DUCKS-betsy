8049d117f77c9e3e5484953a49027746d8aa5e58
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-27 19:16:25 -0700

    I added some stuff and comments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   
-  before_action :require_login, only: [:edit, :update]
+  # I added #new and #create b/c regular guests shouldn't be allowed to add new inventory -CW
+  before_action :require_login, only: [:new, :create, :edit, :update]
+  before_action :require_product_ownership, only: [:edit, :update]
   
   def index 
     @products = Product.where(status: "Available")
@@ -11,6 +13,8 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: product_id)
     
     if @product.nil?
+      # added flash msg so people know why -CW
+      flash[:error] = "Sorry, that product does not currently exist"
       redirect_to root_path
       return
     end
@@ -35,41 +39,59 @@ class ProductsController < ApplicationController
       # refactor so that message will print out nicely instead of with brackets
       @error = @product.errors.messages[:name]
       flash.now[:error] = "Error: #{@error}"
-      render new_product_path
+      # render new_product_path     #commented out b/c crashed -CW
+      render action: "new"
       return
     end
   end
   
   def edit
-    @product = Product.find_by(id: params[:id])
+    # @product = Product.find_by(id: params[:id]) #commented out b/c u already did it in before_action
   end
   
   def update
-    @product = Product.find_by(id: params[:id])
+    # @product = Product.find_by(id: params[:id]) #commented out b/c u already did it in before_action
     
     @status = params[:product][:status]
     
     if @product.update( product_params )
       
       flash[:success] = "You successfully updated #{@product.name}"
-      redirect_to product_path(@product.id)
+      return redirect_to product_path(@product.id)
     else
-      render edit_product_path
+      # render edit_product_path
+      # also added flash msgs -CW
+      flash[:error] = "Unable to update #{@product.name}"
+      flash[:error_msgs] = @product.error.full_messages
+      render action: "edit"
       return
     end
   end
   
   private
+  ### I separated require_login into 2 diff methods b/c SRP -CW
   
   def require_login
     @merchant = Merchant.find_by(id: session[:merchant_id])
+    
+    # what if merchant forgot to login? -CW
+    if @merchant.nil?
+      flash[:error] = "Please log into your merchant account first"
+      return redirect_to root_path
+    end
+  end
+  
+  def require_product_ownership
+    # call this AFTER require_login, in the before_action
     @product = Product.find_by(id: params[:id])
     
     # need to confirm that the product belongs to its merchant
     unless @product.merchant.id == @merchant.id
       flash[:error] = "You are not authorized to edit this product!"
+      # I think u need to render or redirect here, otherwise flash error shows up AFTER u leavet hsi page - CW
+      return redirect_to root_path
     end
-  end
+  end  
   
   def product_params
     return params.require(:product).permit(:name, :price, :stock, :img_url, :description, category_ids: []).merge(status: @status)
