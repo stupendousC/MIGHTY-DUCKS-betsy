5fa46fb39381f2b9cec4c3cd2830baf9c27ad914
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-28 11:05:07 -0700

    Merchant model tests all done

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,8 @@ class Merchant < ApplicationRecord
   has_many :products
   
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # email format regex from stackoverflow.com https://stackoverflow.com/questions/4770133/rails-regex-for-email-validation
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ } 
   
   def self.build_from_github(auth_hash)
     # assembling Merchant.new() using info from github's auth_hash
