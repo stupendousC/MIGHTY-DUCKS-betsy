bd9ac0009b34fd946f677a643bc71c75b3f30f4e
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-28 15:52:05 -0700

    Review controller and form, did tests for product controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homepages#index'
   
-  resources :products
+  resources :products do
+    resources :reviews, only: [:new, :create]
+  end
   
   resources :categories
   
