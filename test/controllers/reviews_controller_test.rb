9b4a45b37c781fab1fd9a58978a3de89e6d95f72
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-30 14:18:52 -0700

    test corrections for review model tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -5,7 +5,7 @@ describe ReviewsController do
     it "A Review Can Be Created For A Product And Responds With Success When Created" do
       product = products(:p1)
       test_review = Review.create(rating: 3, comment:"great product!", product_id: product.id)
-  
+      
       get new_product_review_path(product.id)
       
       must_respond_with :success
@@ -13,16 +13,16 @@ describe ReviewsController do
       expect(test_review.product).must_equal product
       expect(test_review.comment).must_include "great product!"
     end
-
-    it "A Product Can Have Many Reviews" do
-    product = products(:p1)
-    test_review_one = Review.create(rating: 1, comment:"It sucks!", product_id: product.id)
-    test_review_two = Review.create(rating: 5, comment:"wow!", product_id: product.id)
-    get new_product_review_path(product.id)
     
-    must_respond_with :success
-    expect(test_review_one.comment).must_include "It sucks!"
-    expect(test_review_two.comment).must_include "wow"
+    it "A Product Can Have Many Reviews" do
+      product = products(:p1)
+      test_review_one = Review.create(rating: 1, comment:"It sucks!", product_id: product.id)
+      test_review_two = Review.create(rating: 5, comment:"wow!", product_id: product.id)
+      get new_product_review_path(product.id)
+      
+      must_respond_with :success
+      expect(test_review_one.comment).must_include "It sucks!"
+      expect(test_review_two.comment).must_include "wow"
     end
   end 
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -18,30 +18,30 @@ describe Review do
     end
     
     it "Comment Can't be longer than 250 characters" do
-      test_review = Review.create(rating: 4, comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elised do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-        is_valid = test_review.valid?
-        refute(is_valid)
-      end
-      
-      it "Rating can't be Blank" do
-        test_review = Review.create(rating: "", comment: "test comment for review")
-        is_valid = test_review.valid?
-        refute(is_valid)
-      end
-      
-      it "Rating can't be a string" do
-        test_review = Review.create(rating: "hello", comment: "test comment for review")
-        is_valid = test_review.valid?
-        refute(is_valid)
-      end
-      
-      it "will have the required fields" do
-        test_review = Review.create(rating: 2, comment: "test comment for review")
-        [:rating, :comment].each do |field|
-          expect(test_review).must_respond_to field
-        end
+      test_review = Review.create(rating: 4, product: products(:p1), comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elised do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+      is_valid = test_review.valid?
+      refute(is_valid)
+    end
+    
+    it "Rating can't be Blank" do
+      test_review = Review.create(rating: nil, product: products(:p1), comment: "test comment for review")
+      is_valid = test_review.valid?
+      refute(is_valid)
+    end
+    
+    it "Rating can't be a string" do
+      test_review = Review.create(rating: "hello", product: products(:p1), comment: "test comment for review")
+      is_valid = test_review.valid?
+      refute(is_valid)
+    end
+    
+    it "will have the required fields" do
+      test_review = Review.create(rating: 2, product: products(:p1), comment: "test comment for review")
+      [:rating, :comment].each do |field|
+        expect(test_review).must_respond_to field
       end
     end
   end
+end
