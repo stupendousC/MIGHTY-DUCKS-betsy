f5be8a0c6a6b77fb90ba210be57dd8ae29658c31
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-29 20:24:33 -0700

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_195357) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cc_company"
     t.bigint "customer_id"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
