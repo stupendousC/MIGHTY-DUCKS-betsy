88c8213055fd16dd2d90d233f02f8d156ce89ab4
stupendousC <CarolineWuKaplan@gmail.com>
2019-10-30 19:57:13 -0700

    fixed tests due to cc fields migration change

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,125 +1,164 @@
 require "test_helper"
 
 describe ProductsController do
-
+  
+  #   describe "logged-in merchants can" do
+  #     let(:existing_product) {products(:p1)}
+  
+  #     before do
+  #       @m1 = merchants(:m1)
+  #       perform_login(@m1)
+  #     end
+  
+  
+  #     describe "DIANA: #update action" do
+  #     it "can successfully update data on a valid product" do
+  #       puts "\n\n\n\n"
+  #       puts session[:merchant_name]
+  #       puts session[:merchant_name]
+  
+  
+  #       updates = { product: { price: 5000 } }
+  #       puts existing_product.merchant.name
+  
+  #       expect {
+  #       put product_path(existing_product), params: updates
+  #     }.wont_change "Product.count"
+  
+  #     updated_product = Product.find_by(id: existing_product.id)
+  
+  #     # p updated_product.attributes
+  #     # p existing_product.attributes
+  #     p flash
+  
+  #     updated_product.price.must_equal 5000
+  #     must_respond_with :redirect
+  #     must_redirect_to product_path(existing_product.id)
+  
+  #   end
+  # end
+  # end
+  
+  #########
+  
   describe "#index action" do
-    it "responds with success when there is at least one Product saved" do
-      new_product = products(:p1)
-
-      get products_path
-      
-      must_respond_with :success
-      expect(Product.count).must_be :>, 0
-    end
+  it "responds with success when there is at least one Product saved" do
+    new_product = products(:p1)
     
-    it "responds with success when there are products to show " do
-      get products_path
-      
-      must_respond_with :success
-      expect(Product.count).must_equal 4
-    end
+    get products_path
+    
+    must_respond_with :success
+    expect(Product.count).must_be :>, 0
   end
   
-  describe "#show action" do
-    it "responds with success when showing an existing valid product" do
-      new_product = products(:p1)
-      get product_path(new_product.id)
-
-      must_respond_with :success
-      expect(Product.count).must_be :>, 0
-    end
+  it "responds with success when there are products to show " do
+    get products_path
     
-    it "responds with 404 with an invalid product id" do
-      get product_path(-2000)
-
-      assert(flash[:error] == "Sorry! That products doesn't exist.")
-    end
+    must_respond_with :success
+    expect(Product.count).must_equal 4
   end
+end
 
-  describe  "guests can" do
-    describe "#new action" do
-      it "FAIL will not allow and responds with redirect" do
-        get new_product_path
-        
-        must_redirect_to root_path
-        assert(flash[:error] = "You must log-in first")
-      end
-    end
+describe "#show action" do
+it "responds with success when showing an existing valid product" do
+  new_product = products(:p1)
+  get product_path(new_product.id)
+  
+  must_respond_with :success
+  expect(Product.count).must_be :>, 0
+end
 
-    describe "#create action" do
-      it "will not allow and responds with redirect" do
-      get products_path
+it "responds with 404 with an invalid product id" do
+  get product_path(-2000)
+  
+  assert(flash[:error] == "Sorry! That products doesn't exist.")
+end
+end
 
-      must_redirect_to root_path
-      assert(flash[:error] = "You must log-in first")
-      end
-    end
+describe  "guests can" do
+  describe "#new action" do
+  it "FAIL will not allow and responds with redirect" do
+    get new_product_path
+    
+    must_redirect_to root_path
+    assert(flash[:error] = "You must log-in first")
   end
+end
+
+describe "#create action" do
+it "will not allow and responds with redirect" do
+  get products_path
+  
+  must_redirect_to root_path
+  assert(flash[:error] = "You must log-in first")
+end
+end
+end
 
 
-  describe "logged-in merchants can" do
-    let(:existing_product) {products(:p1)}
+describe "logged-in merchants can" do
+  let(:existing_product) {products(:p1)}
+  
+  describe "#new action" do
+  it "creates a new product with valid category data" do
+    m1 = merchants(:m1)
+    perform_login(m1)
+    
+    new_product = { 
+    product: {
+    name: "name",
+    price: 2000,
+    stock: 20,
+    merchant_id: m1.id
+  }
+}
 
-    describe "#new action" do
-      it "creates a new product with valid category data" do
-        m1 = merchants(:m1)
-        perform_login(m1)
+expect {
+post products_path, params: new_product
+}.must_change "Product.count", 1
 
-        new_product = { 
-          product: {
-            name: "name",
-            price: 2000,
-            stock: 20,
-            merchant_id: m1.id
-          }
-        }
+new_product_id = Product.find_by(name: "name").id
 
-        expect {
-          post products_path, params: new_product
-        }.must_change "Product.count", 1
+must_respond_with :redirect
+must_redirect_to product_path(new_product_id)
+end
+end
 
-        new_product_id = Product.find_by(name: "name").id
+describe "#edit action" do
+it "FAIL responds with success for an existing product id" do
+  get edit_product_path(existing_product.id)
+  
+  must_respond_with :success
+end
 
-        must_respond_with :redirect
-        must_redirect_to product_path(new_product_id)
-      end
-    end
+it "FAIL responds with 404 for an invalid product id" do
+  # invalid_id = existing_product.id
+  # existing_product.destroy
+  
+  get edit_product_path(-200)
+  
+  must_respond_with :not_found
+end
+end
 
-    describe "#edit action" do
-      it "FAIL responds with success for an existing product id" do
-        get edit_product_path(existing_product.id)
+describe "#update action" do
+it "can successfully update data on a valid product" do
+  updates = { product: { price: 5000 } }
+  
+  expect {
+  put product_path(existing_product.id), params: updates
+}.wont_change "Product.count"
 
-        must_respond_with :success
-      end
+updated_product = Product.find_by(id: existing_product.id)
 
-      it "FAIL responds with 404 for an invalid product id" do
-        # invalid_id = existing_product.id
-        # existing_product.destroy
+updated_product.price.must_equal 5000
+must_respond_with :redirect
+must_redirect_to product_path(existing_product.id)
 
-        get edit_product_path(-200)
+end
+end
 
-        must_respond_with :not_found
-      end
-    end
-
-    describe "#update action" do
-      it "can successfully update data on a valid product" do
-        updates = { product: { price: 5000 } }
-        
-        expect {
-          put product_path(existing_product.id), params: updates
-        }.wont_change "Product.count"
-        
-        updated_product = Product.find_by(id: existing_product.id)
-
-        updated_product.price.must_equal 5000
-        must_respond_with :redirect
-        must_redirect_to product_path(existing_product.id)
-
-      end
-    end
-
-  end
+end
 
 
 end
