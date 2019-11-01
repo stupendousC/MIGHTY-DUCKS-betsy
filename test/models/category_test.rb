2aea872432298db537973d5f4c9a5e7ae2218c22
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-10-31 19:33:32 -0700

    added tests for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe Category do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "has and can belong to many products" do
+    category = categories(:c1)
+    product_one = products(:p1)
+    product_two = products(:p2)
+    expect(category.products).must_include product_one
+    expect(category.products).must_include product_two
+  end
+
+  it "a product has many categories" do
+    product_one = products(:p1)
+    category_one = categories(:c1)
+    category_two = categories(:c2)
+    category_three = categories(:c3)
+
+    expect(product_one.categories).must_include category_one
+    expect(product_one.categories).must_include category_two
+    expect(product_one.categories).must_include category_three
+  end
+
+  it "must have a unique name" do
+    test_category = Category.create(name: "Toys")
+    is_valid = test_category.valid?
+    refute(is_valid)
+  end
+
+  it "it must have a name, can't be blank" do
+    test_category = Category.create(name: nil)
+    is_valid = test_category.valid?
+    refute(is_valid)
+  end
 end
