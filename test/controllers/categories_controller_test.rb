dff093bf710fdf8d0e5af06f1c306dc74bb04a0e
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-01 11:29:15 -0700

    Tesssssts

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,4 +1,14 @@
 require "test_helper"
 
 describe CategoriesController do
+  
+  describe "show" do 
+    let(:category) { categories(:c1)}
+    it "can get a category by its id" do
+      get category_path(category.id)
+      must_respond_with :success
+      
+    end
+  end
+  
 end
